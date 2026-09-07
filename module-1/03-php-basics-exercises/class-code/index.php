21d535c1eb5f89efd4dd0470468076a1b4b633f7
Rafat Bin <rafati@nait.ca>
2026-09-07 11:49:25 -0600

    added codes

---
+++ module-1/03-php-basics-exercises/class-code/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$title = "Home Page";
+
+include('includes/header.php');
+
+?>
+
+<h1><?= $title ?></h1>
+
+<p class="lead">Welcome to my website!</p>
+
+<a href="#" class="btn btn-primary">Learn More</a>
+
+<?php include('includes/footer.php'); ?>
